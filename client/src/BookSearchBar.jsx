f4ccb0de02d217344dda3197becb3e243f907454
Shakti <shakticse@gmail.com>
2022-01-08 17:21:07 -0500

    fix search bar

--- client/src/BookSearchBar.jsx
+++ client/src/BookSearchBar.jsx
@@ -1,20 +1,27 @@
 import { useEffect, useState } from "react";
 import axios from 'axios';
-import { useBooksApi } from "./hooks/useBookApi";
+import { useBooksApi } from "./hooks/useBookApi.ts";
 
 export default function BookSearchBar() {
   const [testPrintBookApi, setTestPrintBookApi] = useState('');
-  const {getBooksByName} = useBooksApi();
+  const [searchInput, setSearchInput] = useState('');
+
+  const {getBookByName} = useBooksApi();
   useEffect(() => {
   });
+
+  const formatAndGetBooks = () => {
+    
+  }
+
   return (
     <>
       <div>
         <div className="formRow">
           <label>Search books </label>
-          <input type="text" />
+          <input type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
         </div>
-        <button type="submit"> Submit </button>
+        <button type="submit" onClick={() => getBookByName(searchInput)}> Submit </button>
         <h3>The book searched by api is {testPrintBookApi}</h3>
       </div>
     </>
