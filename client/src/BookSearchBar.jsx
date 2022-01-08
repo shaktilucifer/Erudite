3d31ea6f4f77c4fc518679afb7491dc040ec7639
Shakti <shakticse@gmail.com>
2022-01-07 23:38:37 -0500

    add openlibrary api call and  test

--- client/src/BookSearchBar.jsx
+++ client/src/BookSearchBar.jsx
@@ -1,8 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import axios from 'axios';
 
 export default function BookSearchBar() {
+  const [testPrintBookApi, setTestPrintBookApi] = useState('');
   useEffect(() => {
-
+      axios.get('https://openlibrary.org/books/OL7353617M.json')
+      .then((response) => {
+        console.log(response);
+        console.log(response.data.title);
+        setTestPrintBookApi(response.data.title);
+      })
   });
   return (
     <>
@@ -12,6 +19,7 @@ export default function BookSearchBar() {
           <input type="text" />
         </div>
         <button type="submit"> Submit </button>
+        <h3>The book searched by api is {testPrintBookApi}</h3>
       </div>
     </>
   );
