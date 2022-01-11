9bf3d92264953349509ea423da5abbcfa8157fda
Shakti <shakticse@gmail.com>
2022-01-10 22:51:04 -0500

    format document and add loading indicator

--- client/src/BookSearchBar.tsx
+++ client/src/BookSearchBar.tsx
@@ -1,39 +1,48 @@
 import { useEffect, useState } from "react";
-import axios from 'axios';
+import axios from "axios";
 import { useBooksApi } from "./hooks/useBookApi.ts";
 
 export default function BookSearchBar() {
-  const [testPrintBookApi, setTestPrintBookApi] = useState('');
-  const [searchInput, setSearchInput] = useState('');
+  const [testPrintBookApi, setTestPrintBookApi] = useState("");
+  const [searchInput, setSearchInput] = useState("");
   const [bookDocuments, setBookDocuments] = useState([]);
-
-  const {getBookByName} = useBooksApi();
-  useEffect(() => {
-  });
+  const [isLoading, setIsLoading] = useState(false);
+  const { getBookByName } = useBooksApi();
+  useEffect(() => {});
 
   const formatAndGetBooks = (searchInput: string) => {
-       getBookByName(searchInput).then((response) => {
-          if(response) {
-            console.log(response.data.docs);
-            setTestPrintBookApi(searchInput);
-            setBookDocuments(response.data.docs);
-          }
-       });
-  }
+    setIsLoading(true);
+    getBookByName(searchInput)
+      .then((response) => {
+        if (response) {
+          console.log(response.data.docs);
+          setTestPrintBookApi(searchInput);
+          setBookDocuments(response.data.docs);
+        }
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <>
       <div>
         <div className="formRow">
           <label>Search books </label>
-          <input type="text" value={searchInput} onChange={(e) => setSearchInput(e.target.value)} />
+          <input
+            type="text"
+            value={searchInput}
+            onChange={(e) => setSearchInput(e.target.value)}
+          />
         </div>
-        <button type="submit" onClick={() => formatAndGetBooks(searchInput)}> Submit </button>
-        <h3>The book searched by api is {testPrintBookApi}</h3>
+        <button type="submit" onClick={() => formatAndGetBooks(searchInput)}>
+          {!isLoading ? "Submit" : "Processing"}
+        </button>
+        <h3>The book searched by api is: </h3>
         {bookDocuments.map((books) => {
           return <div>{books.title}</div>;
         })}
-        
       </div>
     </>
   );
