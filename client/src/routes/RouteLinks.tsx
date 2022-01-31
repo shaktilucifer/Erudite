858c0548b11b5ee87f1e57a282c67e76a2cea744
Shakti <shakticse@gmail.com>
2022-01-30 21:24:05 -0500

    add capacity to render links for views

---
+++ client/src/routes/RouteLinks.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+export function RouteLinks({appRoutes}) {
+    return (<>
+        {appRoutes.map((route, index) => {
+            return (<li><Link to={route.path}>
+            {route.key}({route.path})
+        </Link> </li>)
+        })}
+        </>
+        )
+}
---
+++ client/src/routes/routeState.ts
@@ -0,0 +1,6 @@
+export interface Route {
+    path: string;
+    key: string;
+    exact: boolean;
+    component: any; // need to change this to something concrete TODO
+}
