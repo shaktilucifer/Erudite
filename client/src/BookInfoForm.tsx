09addc965bd7b69cc809a44d12e890a7f4ab3205
Shakti <shakticse@gmail.com>
2022-02-01 23:31:18 -0500

    add book info form

---
+++ client/src/BookInfoForm.tsx
@@ -0,0 +1,13 @@
+
+export function BookInfoForm() {
+    return (<>
+        <div>
+            <label>Book Name: </label>
+            <input type={'text'} className='formInput' name='bookName' placeholder='enter book name'/>
+        </div>
+        <div>
+            <label>Num pages: </label>
+            <input type={'number'} className='formInput' name='bookName' placeholder='enter number of pages in the book'/>
+        </div>
+    </>)
+}
