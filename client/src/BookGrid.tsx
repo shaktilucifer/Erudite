b0214a3f4c170013a66601e5b76c9a4beaf6db23
Shakti <shakticse@gmail.com>
2022-02-02 20:45:47 -0500

    book grid

---
+++ client/src/BookGrid.tsx
@@ -0,0 +1,10 @@
+
+export function BookGrid() {
+
+    useEffect(() =>{
+        console.log('get list of books , pages read by day');
+        console.log('probably get this when user logs the data in book journal');
+    })
+
+    return (<div></div>)
+}
