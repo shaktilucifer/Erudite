eb9292342267f972ba43e6dd6afd87540993c163
Shakti <shakticse@gmail.com>
2022-02-07 23:40:30 -0500

    add dayblcok

--- client/src/BookGrid.tsx
+++ client/src/BookGrid.tsx
@@ -1,17 +1,14 @@
 import React, { useEffect } from 'react';
 import './BookGrid.css';
+import { DayBlock } from './DayBlock';
 
 export function BookGrid() {
 
     useEffect(() =>{
-        console.log('get list of books , pages read by day');
-        console.log('probably get this when user logs the data in book journal');
-    })
-
-    const renderDayBlocks = () => {
-        return (<div className='daysBlock'></div>);
-    }
+        // console.log('get list of books , pages read by day');
+        // console.log('probably get this when user logs the data in book journal');
+    });
 
 
-    return (<div className='daysWrapper'>{renderDayBlocks()}</div>)
+    return (<div className='daysWrapper'><DayBlock /></div>)
 }
