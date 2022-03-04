82b3e70aa3081cfc4481715f8a8069da19d40d00
Shakti <shakticse@gmail.com>
2022-03-03 23:56:55 -0500

    add visibility to tooltip

--- client/src/Tooltip.tsx
+++ client/src/Tooltip.tsx
@@ -1,7 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 
 export function ToolTip({title, description}) {
-
+    const [visible, setVisible] = useState();
     useEffect(() =>{
     }, []);
 
