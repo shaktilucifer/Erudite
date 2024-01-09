de457ed90f36ea0f5ff1dabdacfa26ec5b4d335d
shaktilucifer <shakticse@gmail.com>
2024-01-08 21:42:38 -0500

    add types for daily book data

--- erudite/src/Erudite.tsx
+++ erudite/src/Erudite.tsx
@@ -1,16 +1,33 @@
-import { useState }  from 'react';
-import './Erudite.css';
+import { useEffect, useState } from "react";
+import "./Erudite.css";
 
-export function Erudite() { 
-  let [data, setData] = useState();
-  data = [
-        {name: "Mark_of_Athena", pageInfo: {start_end: [113, 165]}}, 
-        {name: "Mark_of_Athena", pageInfo: {start_end: [166, 219]}} 
-        ];
-  return (
-    <>
-        {data.map((d) => <div>{d.name}: {`${d.pageInfo.start_end[0]} - ${d.pageInfo.start_end[1]}` }</div>)}
-    </>
-  )
+interface DailyBookData {
+  id: string | number;
+  name: string;
+  pageInfo: PageInfo;
 }
 
+interface PageInfo {
+  start: number;
+  end: number;
+}
+
+export function Erudite() {
+  let [data, setData] = useState<DailyBookData[]>([]);
+
+  useEffect(() => {
+    setData([
+      { id: "1", name: "Mark_of_Athena", pageInfo: { start: 113, end: 165 } },
+      { id: "1", name: "Mark_of_Athena", pageInfo: { start: 166, end: 219 } },
+    ]);
+  });
+  return (
+    <>
+      {data.map((d) => (
+        <div>
+          {d.name}: {`${d.pageInfo.start} - ${d.pageInfo.end}`}
+        </div>
+      ))}
+    </>
+  );
+}
