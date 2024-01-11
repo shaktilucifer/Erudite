fb9d495541f5c5d31132031ed001168b3ec2a411
shaktilucifer <shakticse@gmail.com>
2024-01-10 20:04:57 -0500

    mock global store

--- erudite/src/hooks/useCreateBook.ts
+++ erudite/src/hooks/useCreateBook.ts
@@ -1,7 +1,27 @@
 import { useCallback } from "react";
 
+interface Book {
+    name: string;
+    id: number | string;
+    pages: number;
+    author: string;
+    ISBN?: string;
+}
+
+const someGlobalMock: Book[] = [{
+    name: 'Mark of Athena',
+    id: 1,
+    pages: 586,
+    author: 'Rick Riordan',
+}];
+
 export function useCreateBook(){
-    return useCallback(() => {
+    return useCallback((book: Book) => {
             // do call to save book
+            someGlobalMock.push(book);
     }, []);
+}
+
+export function getGlobalBookStore() {
+    return someGlobalMock;
 }
