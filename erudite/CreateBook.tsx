65a3866c4f0f644d67b4fa58f3d51d34fb51879b
shaktilucifer <shakticse@gmail.com>
2024-01-11 20:13:48 -0500

    book entry form

--- erudite/CreateBook.tsx
+++ erudite/CreateBook.tsx
@@ -1,12 +1,30 @@
 // Create book entry with isbn and other details
 // select books from this to be shown in book Entry form
-import React from 'react';
+import React, { useState } from "react";
 import "./CreateBook.less";
 
-interface CreateBookProps {
-    
-}
+interface CreateBookProps {}
 
 export function CreateBook({}: CreateBookProps) {
-  return <></>;
+  const [bookName, setBookName] = useState("");
+  const [page_count, setPageCount] = useState<number>();
+  
+  return (
+    <>
+      Name:
+      <input
+        type="text"
+        name="book_name"
+        value={bookName}
+        onChange={(e) => setBookName(e.target.value)}
+      />
+      Pages:
+      <input
+        type="number"
+        name="page_count"
+        value={page_count}
+        onChange={(e) => setPageCount(+e.target.value || 0)}
+      />
+    </>
+  );
 }
