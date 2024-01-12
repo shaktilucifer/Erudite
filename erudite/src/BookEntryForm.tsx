35745b9dcbf80b212f43ae8a8ebeb6fd6c21c662
shaktilucifer <shakticse@gmail.com>
2024-01-11 20:17:14 -0500

    on change for book entry form

--- erudite/src/BookEntryForm.tsx
+++ erudite/src/BookEntryForm.tsx
@@ -1,17 +1,30 @@
-import { useEffect, useState } from "react";
-import './BookEntryForm.less';
+import { useState } from "react";
+import "./BookEntryForm.less";
 import { DailyBookData } from "./Erudite";
 
+export function BookDataEntryForm({}) {
+  const [bookInfo, setBookInfo] = useState<DailyBookData>({
+    name: "",
+    id: 0,
+    pageInfo: { start: 0, end: 0 },
+  });
 
-export function BookEntryForm({}) {
-    const [bookInfo, setBookInfo] = useState<DailyBookData>();
-
-    return 
-    <>
-        {/* // add on change for these  */}
-        Book name: <input key={'book_name'} type="text" value={bookInfo?.name} />
-        Start: <input key={'start_page'} type="text" value={bookInfo?.pageInfo.start} />
-        End: <input key={'end_page'} type="text" value={bookInfo?.pageInfo.end} />
-
-    </>
+  return;
+  <>
+    {/* // add on change for these  */}
+    Book name:{" "}
+    <input
+      key={"book_name"}
+      type="text"
+      value={bookInfo?.name}
+      onChange={(e) =>
+        setBookInfo((prev: DailyBookData) => {
+          return { ...prev, name: e.target.value };
+        })
+      }
+    />
+    Start:{" "}
+    <input key={"start_page"} type="text" value={bookInfo?.pageInfo.start} />
+    End: <input key={"end_page"} type="text" value={bookInfo?.pageInfo.end} />
+  </>;
 }
