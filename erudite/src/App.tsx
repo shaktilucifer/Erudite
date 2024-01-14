5f71f18e7828c0846cf59f729298855adf286dba
shaktilucifer <shakticse@gmail.com>
2024-01-13 23:05:21 -0500

    Update BookEntryForm

--- erudite/src/App.tsx
+++ erudite/src/App.tsx
@@ -1,11 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import {Erudite} from './Erudite.tsx'
+import { useState } from "react";
+import "./App.css";
+import { Erudite } from "./Erudite.tsx";
+import { BookDataEntryForm } from "./BookEntryForm.tsx";
 
 function App() {
   return (
-  <> <Erudite />    </>
-  )
+    <>
+      <Erudite />
+      <BookDataEntryForm />
+    </>
+  );
 }
 
-export default App
+export default App;
--- erudite/src/BookEntryForm.tsx
+++ erudite/src/BookEntryForm.tsx
@@ -2,28 +2,32 @@ import { useState } from "react";
 import "./BookEntryForm.less";
 import { DailyBookData } from "./Erudite";
 
-export function BookDataEntryForm({}) {
+export interface BookDataEntryFormProps {}
+
+export function BookDataEntryForm({}: BookDataEntryFormProps) {
   const [bookInfo, setBookInfo] = useState<DailyBookData>({
     name: "",
     id: 0,
     pageInfo: { start: 0, end: 0 },
   });
 
-  return;
-  <>
-    {/* // add on change for these  */}
-    Book name:{" "}
-    <input
-      key={"book_name"}
-      type="text"
-      value={bookInfo?.name}
-      onChange={(e) =>
-        setBookInfo((prev: DailyBookData) => {
-          return { ...prev, name: e.target.value };
-        })
-      }
-    />
-    Start:<input key={"start_page"} type="text" value={bookInfo?.pageInfo.start} />
-    End: <input key={"end_page"} type="text" value={bookInfo?.pageInfo.end} />
-  </>;
+  return (
+    <>
+      {/* // add on change for these  */}
+      Book name:{" "}
+      <input
+        key={"book_name"}
+        type="text"
+        value={bookInfo?.name}
+        onChange={(e) =>
+          setBookInfo((prev: DailyBookData) => {
+            return { ...prev, name: e.target.value };
+          })
+        }
+      />
+      Start:
+      <input key={"start_page"} type="text" value={bookInfo?.pageInfo.start} />
+      End: <input key={"end_page"} type="text" value={bookInfo?.pageInfo.end} />
+    </>
+  );
 }
