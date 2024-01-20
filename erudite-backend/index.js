abaf27181e02129f628b6fd2411911bbf848db8c
shaktilucifer <shakticse@gmail.com>
2024-01-19 22:22:29 -0500

    add endpoint for getting and saving book

--- erudite-backend/index.js
+++ erudite-backend/index.js
@@ -6,9 +6,15 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/create-book", (req, res) => {
+app.get("/get-book", (req, res) => {
+  // get from db
     res.json({test: "book created!"})
-})
+});
+
+app.put("/create-book", (req, res) => {
+  // save to db
+    res.json({test: "book created!"})
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
