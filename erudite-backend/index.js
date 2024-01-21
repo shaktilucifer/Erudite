6580c1ecb0e00c89264b10ca8bc0af9cdf4618b3
shaktilucifer <shakticse@gmail.com>
2024-01-20 22:38:06 -0500

    Add endpoints with mock book data

--- erudite-backend/index.js
+++ erudite-backend/index.js
@@ -6,12 +6,36 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-app.get("/get-book", (req, res) => {
+const books =  [{
+  name: 'Mark of Athena',
+  id: 1,
+  pages: 586,
+  author: 'Rick Riordan',
+}]
+
+app.get("/get-books", (req, res) => {
   // get from db
-    res.json({test: "book created!"})
+    res.json({books})
 });
 
-app.put("/create-book", (req, res) => {
+
+app.get("/getBookById", (req, res) => {
+  const filteredBooks = books.filter(book => book.id === req.id);
+  // get from db
+
+    res.json({books: filteredBooks});
+});
+
+app.get("/getBookByName", (req, res) => {
+  const filteredBooks = books.filter(book => book.name === req.name);
+  // get from db
+
+    res.json({books: filteredBooks});
+});
+
+
+app.put("/createBook", (req, res) => {
+  books.push(req.book);
   // save to db
     res.json({test: "book created!"})
 });
