62954f690733db9b3bcd66de7252a0bba8ce978f
shaktilucifer <shakticse@gmail.com>
2024-01-20 22:38:59 -0500

    update mock data

--- erudite-backend/index.js
+++ erudite-backend/index.js
@@ -6,38 +6,44 @@ const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-const books =  [{
-  name: 'Mark of Athena',
-  id: 1,
-  pages: 586,
-  author: 'Rick Riordan',
-}]
+const books = [
+  {
+    name: "Mark of Athena",
+    id: 1,
+    pages: 586,
+    author: "Rick Riordan",
+  },
+  {
+    name: "Heroes of Olympus",
+    id: 2,
+    pages: 344,
+    author: "Rick Riordan",
+  },
+];
 
 app.get("/get-books", (req, res) => {
   // get from db
-    res.json({books})
+  res.json({ books });
 });
 
-
 app.get("/getBookById", (req, res) => {
-  const filteredBooks = books.filter(book => book.id === req.id);
+  const filteredBooks = books.filter((book) => book.id === req.id);
   // get from db
 
-    res.json({books: filteredBooks});
+  res.json({ books: filteredBooks });
 });
 
 app.get("/getBookByName", (req, res) => {
-  const filteredBooks = books.filter(book => book.name === req.name);
+  const filteredBooks = books.filter((book) => book.name === req.name);
   // get from db
 
-    res.json({books: filteredBooks});
+  res.json({ books: filteredBooks });
 });
-
 
 app.put("/createBook", (req, res) => {
   books.push(req.book);
   // save to db
-    res.json({test: "book created!"})
+  res.json({ test: "book created!" });
 });
 
 app.listen(PORT, () => {
