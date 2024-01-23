de27c4061f0356a7eec9999d15f46757c6fdeede
shaktilucifer <shakticse@gmail.com>
2024-01-22 21:54:50 -0500

    update main app with daily entry

--- erudite/src/App.tsx
+++ erudite/src/App.tsx
@@ -2,17 +2,25 @@ import "./App.css";
 import { DailyBookData, Erudite } from "./Erudite.tsx";
 import { BookDataEntryForm } from "./BookEntryForm.tsx";
 import { CreateBook } from "./CreateBook.tsx";
+import { useState } from "react";
 
 function App() {
+  const [dailyBookData, setDailyBookData] = useState<DailyBookData[]>([]);
   return (
     <>
-      <div><Erudite /></div>
-      <div><CreateBook /></div>
-      <div><BookDataEntryForm
-        onSubmit={(dailyBookData: DailyBookData) =>
-          "not implemented" + dailyBookData
-        }
-      /></div>
+      <div>
+        <Erudite dailyBookData={dailyBookData} />
+      </div>
+      <div>
+        <CreateBook />
+      </div>
+      <div>
+        <BookDataEntryForm
+          onSubmit={(dailyBookData: DailyBookData) => {
+            setDailyBookData([dailyBookData]);
+          }}
+        />
+      </div>
     </>
   );
 }
--- erudite/src/Erudite.tsx
+++ erudite/src/Erudite.tsx
@@ -21,10 +21,7 @@ export function Erudite({dailyBookData = []}: EruditeProps) {
   let [data, setData] = useState<DailyBookData[]>(dailyBookData);
   // test data
   useEffect(() => {
-    setData([
-      { id: "1", name: "Mark_of_Athena", pageInfo: { start: 113, end: 165 }, date: '11/11/2019' },
-      { id: "1", name: "Mark_of_Athena", pageInfo: { start: 166, end: 219 }, date: '11/12/2022' },
-    ]);
+
   }, []);
   return (
     <>
