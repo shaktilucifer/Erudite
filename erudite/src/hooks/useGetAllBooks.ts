713478fc777968b2a54cb0f4faa10ef41d359064
shaktilucifer <shakticse@gmail.com>
2024-02-06 00:38:53 -0500

    Add new create browser router and routes

--- erudite/src/hooks/useGetAllBooks.ts
+++ erudite/src/hooks/useGetAllBooks.ts
@@ -1,13 +1,13 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Book } from "./useCreateBook";
 import { ApiClient } from "../utils/ApiClient";
 
 const apiClient = new ApiClient();
 
-export function useGetAllBooks(): {books: Book[]} {
+export function useGetAllBooks(): { books: Book[] } {
   const [books, setBooks] = useState<Book[]>([]);
   useEffect(() => {
-    apiClient.get("/api/getAllBooks").then((response: {books: Book[]}) => {
+    apiClient.get("/api/getAllBooks").then((response: { books: Book[] }) => {
       setBooks(response.books);
     });
   }, []);
--- erudite/src/main.tsx
+++ erudite/src/main.tsx
@@ -1,13 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import { BrowserRouter } from "react-router-dom";
+import {
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements,
+} from "react-router-dom";
 import "./index.css";
+import { BookList } from "./BookList.tsx";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path='/' element={<App />} />
+      <Route path="/books" element={<BookList />} />
+    </>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
